6d3de00bb5c2a785c5db1462a034a44ceb6324a4
SujalSinha12012005 <sujalsinha2005@gmail.com>
2026-07-30 03:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (12.64%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+       if(n<=0) return false ; 
+       int result = n&(n-1); 
+        if(result ==0)return true ;
+        return false ; 
+    }
+};
